d9621988ce7b7de968f1b6df47b586fa5cb6e64a
Fernanda Ávila <153337144+fernanda-avila@users.noreply.github.com>
2024-09-22 22:25:33 -0300

    Add files via upload

--- users.js
+++ users.js
@@ -19,6 +19,34 @@ const users = [
         "birthDate": "07111997",
         "phone": "53991968011",
         "isAdult": true
+    },
+    {
+        "email": "carolsoaresfreitas@hotmail.com",
+        "password": "$2a$10$YdjHyz2.WjAEHNOHh6R5J.ikgIt8mVacspd98boIlzIZFN4VeNVl2",
+        "birthDate": "09022001",
+        "phone": "5390909090",
+        "isAdult": true
+    },
+    {
+        "email": "fernandavila987@gmail.com",
+        "password": "$2a$10$aUp2XgdvehMqL.KSFxGZ0uAtLDPe5sXbmgykaxxy410QTzYAF7wZ6",
+        "birthDate": "07111997",
+        "phone": "53991986011",
+        "isAdult": true
+    },
+    {
+        "email": "clear",
+        "password": "$2a$10$47d02E7hwIngttoKqx3fLOl2quSd/Qr0GEGX31e2ZviMNvmCClT0y",
+        "birthDate": "07091998",
+        "phone": "53991919191",
+        "isAdult": false
+    },
+    {
+        "email": "avilafernanda123@gmail.com",
+        "password": "$2a$10$q3TkLBOk7hOOAjq8pxCo2eY217mBYYiOju8AUSXlCnDrShUGWTEXu",
+        "birthDate": "07111997",
+        "phone": "5399191919191",
+        "isAdult": true
     }
 ];
 
